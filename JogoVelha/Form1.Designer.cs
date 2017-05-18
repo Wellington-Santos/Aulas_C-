044e654353a84e931a668f934fc3bdc49dbd2f15
internet <internet@WKT-104912>
2017-05-18 19:48:50 -0300

    Add project files.

---
+++ JogoVelha/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace JogoVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.B11 = new System.Windows.Forms.Button();
+            this.B12 = new System.Windows.Forms.Button();
+            this.B13 = new System.Windows.Forms.Button();
+            this.B23 = new System.Windows.Forms.Button();
+            this.B22 = new System.Windows.Forms.Button();
+            this.B21 = new System.Windows.Forms.Button();
+            this.B33 = new System.Windows.Forms.Button();
+            this.B32 = new System.Windows.Forms.Button();
+            this.B31 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // B11
+            // 
+            this.B11.Location = new System.Drawing.Point(37, 70);
+            this.B11.Name = "B11";
+            this.B11.Size = new System.Drawing.Size(50, 50);
+            this.B11.TabIndex = 0;
+            this.B11.UseVisualStyleBackColor = true;
+            // 
+            // B12
+            // 
+            this.B12.Location = new System.Drawing.Point(93, 70);
+            this.B12.Name = "B12";
+            this.B12.Size = new System.Drawing.Size(50, 50);
+            this.B12.TabIndex = 1;
+            this.B12.UseVisualStyleBackColor = true;
+            // 
+            // B13
+            // 
+            this.B13.Location = new System.Drawing.Point(149, 70);
+            this.B13.Name = "B13";
+            this.B13.Size = new System.Drawing.Size(50, 50);
+            this.B13.TabIndex = 2;
+            this.B13.UseVisualStyleBackColor = true;
+            // 
+            // B23
+            // 
+            this.B23.Location = new System.Drawing.Point(149, 126);
+            this.B23.Name = "B23";
+            this.B23.Size = new System.Drawing.Size(50, 50);
+            this.B23.TabIndex = 5;
+            this.B23.UseVisualStyleBackColor = true;
+            // 
+            // B22
+            // 
+            this.B22.Location = new System.Drawing.Point(93, 126);
+            this.B22.Name = "B22";
+            this.B22.Size = new System.Drawing.Size(50, 50);
+            this.B22.TabIndex = 4;
+            this.B22.UseVisualStyleBackColor = true;
+            // 
+            // B21
+            // 
+            this.B21.Location = new System.Drawing.Point(37, 126);
+            this.B21.Name = "B21";
+            this.B21.Size = new System.Drawing.Size(50, 50);
+            this.B21.TabIndex = 3;
+            this.B21.UseVisualStyleBackColor = true;
+            // 
+            // B33
+            // 
+            this.B33.Location = new System.Drawing.Point(149, 182);
+            this.B33.Name = "B33";
+            this.B33.Size = new System.Drawing.Size(50, 50);
+            this.B33.TabIndex = 8;
+            this.B33.UseVisualStyleBackColor = true;
+            // 
+            // B32
+            // 
+            this.B32.Location = new System.Drawing.Point(93, 182);
+            this.B32.Name = "B32";
+            this.B32.Size = new System.Drawing.Size(50, 50);
+            this.B32.TabIndex = 7;
+            this.B32.UseVisualStyleBackColor = true;
+            // 
+            // B31
+            // 
+            this.B31.Location = new System.Drawing.Point(37, 182);
+            this.B31.Name = "B31";
+            this.B31.Size = new System.Drawing.Size(50, 50);
+            this.B31.TabIndex = 6;
+            this.B31.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 39);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "X, é a sua vez";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 237);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.B33);
+            this.Controls.Add(this.B32);
+            this.Controls.Add(this.B31);
+            this.Controls.Add(this.B23);
+            this.Controls.Add(this.B22);
+            this.Controls.Add(this.B21);
+            this.Controls.Add(this.B13);
+            this.Controls.Add(this.B12);
+            this.Controls.Add(this.B11);
+            this.Name = "Form1";
+            this.Text = "Velha";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button B11;
+        private System.Windows.Forms.Button B12;
+        private System.Windows.Forms.Button B13;
+        private System.Windows.Forms.Button B23;
+        private System.Windows.Forms.Button B22;
+        private System.Windows.Forms.Button B21;
+        private System.Windows.Forms.Button B33;
+        private System.Windows.Forms.Button B32;
+        private System.Windows.Forms.Button B31;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ JogoVelha/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoVelha
+{
+    public partial class Form1 : Form
+    {
+        bool xis = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            B11.Click += new EventHandler(BClick);
+            B12.Click += new EventHandler(BClick);
+            B13.Click += new EventHandler(BClick);
+            B21.Click += new EventHandler(BClick);
+            B22.Click += new EventHandler(BClick);
+            B23.Click += new EventHandler(BClick);
+            B31.Click += new EventHandler(BClick);
+            B32.Click += new EventHandler(BClick);
+            B33.Click += new EventHandler(BClick);
+
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button)
+                {
+                    item.TabStop = false;
+                }
+            }
+        }
+
+        private void BClick(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = this.xis ? "X" : "O";
+            ((Button)sender).Enabled = false;
+
+            VerificarGanhador();
+            xis = !xis;
+            label1.Text = string.Format("{0}, é a sua vez", this.xis ? "X" : "O");
+        }
+
+        private void VerificarGanhador()
+        {
+            if (
+                B11.Text != String.Empty && B11.Text == B12.Text && B12.Text == B13.Text || //Linha 1
+                B21.Text != String.Empty && B21.Text == B22.Text && B22.Text == B23.Text || //Linha 2
+                B31.Text != String.Empty && B31.Text == B32.Text && B32.Text == B33.Text || //Linha 3  
+
+                B11.Text != String.Empty && B11.Text == B22.Text && B22.Text == B33.Text || //Linha \  
+                B13.Text != String.Empty && B13.Text == B22.Text && B22.Text == B31.Text  //Linha /
+              )
+            {
+                MessageBox.Show(String.Format("O ganhador é o [{0}]", xis ? "X" : "O"), "Temos um Vitorio",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Reiniciar();
+            }
+            else
+            {
+                VerificarEmpate();
+            }
+        }
+
+        private void VerificarEmpate()
+        {
+            bool todosDesabilitados = true;
+
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button && item.Enabled)
+                {
+                    todosDesabilitados = false;
+                    break;
+                }
+            }
+
+            if (todosDesabilitados)
+            {
+                MessageBox.Show(String.Format("Deu empate"), "Ops!!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Reiniciar();
+            }
+        }
+
+        private void Reiniciar()
+        {
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button)
+                {
+                    item.Enabled = true;
+                    item.Text = String.Empty;
+                }
+            }
+        }
+    }
+}
